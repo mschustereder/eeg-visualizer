7a2dc17b48ff1b1e6a34a203c912b6c0b5fd467f
Mathias Schustereder <schustereder@student.tugraz.at>
2024-05-31 22:24:32 +0200

    Allow adding sub plots dynamically

--- visualizer/eeg_visualizer.py
+++ visualizer/eeg_visualizer.py
@@ -34,7 +34,7 @@ main_plot_container = dbc.Card(
 )
 
 configuration_for_main_plot = [
-    html.H5("Configure Main Plot", className="card-title"),
+    html.H5("Configure main plot", className="card-title"),
     html.Div([
         'Choose main plot',
         dcc.Dropdown(['Topoplot', 'Spectrogram'], 'Spectrogram', id='main-plot-selection', clearable=False)
@@ -53,7 +53,6 @@ def get_configuration_for_sub_plot(index):
 
 configuration_list = dbc.ListGroup(id="configuration-list", children=[
     dbc.ListGroupItem(children=configuration_for_main_plot),
-    dbc.ListGroupItem(children=get_configuration_for_sub_plot(0))
 ])
 
 buttons_list_item = [
@@ -68,11 +67,14 @@ buttons_for_plot_configuration = dbc.ListGroup(dbc.ListGroupItem(children=button
 
 button_clicks_store = dcc.Store(id='button_clicks_store', data={'last_click_add_button': 0, 'last_click_remove_button': 0})
 
+add_sub_plot_store = dcc.Store(id='add_sub_plot_store', data={'just_a_counter': 0, 'sub_plot_index': -17})
+
 configuration_area = dbc.Card(
     [
         dbc.CardBody(
             [
                 button_clicks_store,
+                add_sub_plot_store,
                 html.H4("Configuration", className="card-title"),
                 configuration_list,
                 buttons_for_plot_configuration
@@ -88,7 +90,7 @@ def get_sub_plot(index):
                 [
                     dbc.CardBody(
                         [
-                            html.H4(f"Sub Plot {index}", className="card-title", id=f'sub-plot-title-{index}'),
+                            html.H4(f"Sub Plot {index + 1}", className="card-title", id=f'sub-plot-title-{index}'),
                             html.P(
                                 "Here there will be some form of line plot. ECG for example, or RR Plot.",
                                 className="card-text",
@@ -142,17 +144,15 @@ draggable_layout = dash_draggable.GridLayout(
                 "i": "modal",
                 "x": 11, "w": 2, "y": 12, "h": 2
             },
-            get_layout_for_sub_plot(0),
         ],
     children=[
         main_plot_container,
         configuration_area,
-        get_sub_plot(0),
         html.Div(children=[
                 lsl_stream_selection_modal
             ],
             id='modal'
-        )
+        ),
     ],
     style={
         "border":"2px solid blue",
@@ -188,33 +188,61 @@ def update_auxiliary_plot(value):
 @app.callback(
     Output("configuration-list", "children"),
     Output('button_clicks_store', 'data'),
-    Output('add-sub-plot-button', 'disabled'),
-    Output('remove-sub-plot-button', 'disabled'),
+    Output('add_sub_plot_store', 'data'),
+    # Output('add-sub-plot-button', 'disabled'),
+    # Output('remove-sub-plot-button', 'disabled'),
     [Input("add-sub-plot-button", "n_clicks"), Input("remove-sub-plot-button", "n_clicks")],
     State('button_clicks_store', 'data'),
     State('configuration-list', 'children'),
+    State('add_sub_plot_store', 'data')
+    # State('draggable', 'children'),
+    # State('draggable', 'layout'),
 )
-def update_plot_configuration(num_clicks_add_button, num_clicks_remove_button, stored_clicks_data, children_configuration_list):
+def update_plot_configuration(num_clicks_add_button, num_clicks_remove_button, stored_clicks_data, children_configuration_list, add_sub_plot_store_data):
     def was_button_clicked(num_clicks, last_click): return num_clicks > last_click
 
     add_button_clicked = was_button_clicked(num_clicks_add_button, stored_clicks_data['last_click_add_button'])
     remove_button_clicked = was_button_clicked(num_clicks_remove_button, stored_clicks_data['last_click_remove_button'])
     
-    assert add_button_clicked is False or remove_button_clicked is False
-
     update_value_for_store = {'last_click_add_button': num_clicks_add_button, 'last_click_remove_button': num_clicks_remove_button}
+
+    # if add_button_clicked is True and remove_button_clicked is True:
+    #     return no_update, update_value_for_store, no_update, no_update, no_update, no_update
 
     if add_button_clicked:
         sub_plot_index = len(children_configuration_list) - 1
         new_children_for_configuration_list = children_configuration_list + [dbc.ListGroupItem(children=get_configuration_for_sub_plot(sub_plot_index))]
-        should_disable_add_button = True if len(new_children_for_configuration_list) >= gl.MAX_NUM_OF_PLOTS else False
-        return new_children_for_configuration_list, update_value_for_store, should_disable_add_button, False
+        # new_children_for_draggable = children_draggable + [get_sub_plot(sub_plot_index)]
+        # new_layout_for_draggable = layout_draggable + [get_layout_for_sub_plot(sub_plot_index)]
+        # should_disable_add_button = True if len(new_children_for_configuration_list) >= gl.MAX_NUM_OF_PLOTS else False
+        # return new_children_for_configuration_list, update_value_for_store, should_disable_add_button, no_update, new_children_for_draggable, new_layout_for_draggable
+        update_value_for_add_sub_plot_store = {'just_a_counter': add_sub_plot_store_data['just_a_counter'], 'sub_plot_index': sub_plot_index}
+        return new_children_for_configuration_list, update_value_for_store, update_value_for_add_sub_plot_store
+        # return new_children_for_configuration_list, update_value_for_store, new_children_for_draggable, new_layout_for_draggable
     elif remove_button_clicked:
         new_children_for_configuration_list = children_configuration_list[:-1]
-        should_disable_remove_button = True if len(new_children_for_configuration_list) <= 1 else False
-        return new_children_for_configuration_list, update_value_for_store, False, should_disable_remove_button
+        # new_children_for_draggable = children_draggable[:-1]
+        # new_layout_for_draggable = layout_draggable[:-1]
+        # should_disable_remove_button = True if len(new_children_for_configuration_list) <= 1 else False
+        # return new_children_for_configuration_list, update_value_for_store, no_update, should_disable_remove_button, new_children_for_draggable, new_layout_for_draggable
+        return new_children_for_configuration_list, update_value_for_store, no_update
+        # return new_children_for_configuration_list, update_value_for_store, new_children_for_draggable, new_layout_for_draggable
     else:
-        return no_update, update_value_for_store, False, False
+        return no_update, update_value_for_store, no_update
+    
+@app.callback(
+    Output('draggable', 'children'),
+    Output('draggable', 'layout'),
+    Input('add_sub_plot_store', 'data'),
+    State('draggable', 'children'),
+    State('draggable', 'layout'),
+)
+def add_plots_and_layouts(data_in_add_sub_plot_store, children_draggable, layout_draggable):
+    print(f"The data in the add_sub_plot_store changed! It is now: {data_in_add_sub_plot_store}")
+    sub_plot_index = data_in_add_sub_plot_store['sub_plot_index']
+    new_children_for_draggable = children_draggable + [get_sub_plot(sub_plot_index)]
+    new_layout_for_draggable = layout_draggable + [get_layout_for_sub_plot(sub_plot_index)]
+    return new_children_for_draggable, new_layout_for_draggable
 
 @app.callback(
     Output("lsl_streams_list", "children"),
