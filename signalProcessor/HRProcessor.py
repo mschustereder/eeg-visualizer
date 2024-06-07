bb787089116fbe45beae1d5c923b64a3b3166abc
Felix Gaisberger <f.gaisberger@student.tugraz.at>
2024-06-07 17:07:57 +0200

    change hrprocessor to store past values

--- signalProcessor/HRProcessor.py
+++ signalProcessor/HRProcessor.py
@@ -4,6 +4,7 @@ from hrvanalysis import get_time_domain_features, get_poincare_plot_features
 from hrvanalysis.preprocessing import interpolate_nan_values, remove_ectopic_beats
 import numpy as np
 from collections import deque
+from ..visualizer.VisualizerHR import HR_BIO_VARIABLE
 
 #Code with (ref) taken from https://github.com/abcsds/hrv/blob/main/src/09b_plot_hrv.py - Author: Luis Alberto Barradas Chacón
 
@@ -16,17 +17,22 @@ rr_data_buffer = deque([average_rr] * MAX_DATA_POINTS, maxlen=MAX_DATA_POINTS)
 
 
 class HRProcessor:
-    def __init__(self, lslhandler: LslHandler, hr_stream : StreamInfo, rr_stream : StreamInfo):
+    def __init__(self, lslhandler: LslHandler, hr_stream : StreamInfo, rr_stream : StreamInfo, storage_size: int):
         self.lslhandler = lslhandler
         self.hr_stream = hr_stream
         self.rr_stream = rr_stream
+        self.bpm_buffer = []
+        self.rmssd_buffer = []
+        self.sdnn_buffer = []
+        self.poincare_buffer = []
+        self.storage_size = storage_size
 
     lslhandler : LslHandler
     hr_stream : StreamInfo
     rr_stream : StreamInfo
 
-    def get_bpm_data(self):
-        return self.lslhandler.get_data_sample(self.hr_stream)
+    def update_bpm(self):
+        self.bpm_buffer.append(self.lslhandler.get_data_sample(self.hr_stream))
         
     def _get_rr_intervals(self):
         #(ref)
@@ -42,26 +48,46 @@ class HRProcessor:
             return rr_intervals
         return None
     
-    def get_rmssd_data(self):
-        rr_intervals = self._get_rr_intervals()
-        if rr_intervals != None:
-            time_feats = get_time_domain_features(nn_intervals=rr_intervals)
-            return time_feats['rmssd']
-        return None
+    def update_rmssd(self, rr_intervals):
+        time_feats = get_time_domain_features(nn_intervals=rr_intervals)
+        self.rmssd_buffer.append(time_feats['rmssd'])
 
-    def get_sdnn_data(self):
-        rr_intervals = self._get_rr_intervals()
-        if rr_intervals != None:
-            time_feats = get_time_domain_features(nn_intervals=rr_intervals)
-            return time_feats['sdnn']
-        return None
+    def update_sdnn(self, rr_intervals):
+        time_feats = get_time_domain_features(nn_intervals=rr_intervals)
+        self.sdnn_buffer.append(time_feats['sdnn'])
     
-    def get_poincare_ratio(self):
+    def update_poincare(self, rr_intervals):
+        poincare_feats = get_poincare_plot_features(nn_intervals=rr_intervals)
+        self.poincare_buffer.append(poincare_feats['ratio_sd2_sd1'])
+
+    def check_buffer_sizes(self):
+        if len(self.bpm_buffer) >= self.storage_size:
+            self.bpm_buffer = self.bpm_buffer[1:]
+            self.poincare_buffer = self.poincare_buffer[1:]
+            self.sdnn_buffer = self.sdnn_buffer[1:]
+            self.rmssd_buffer = self.rmssd_buffer[1:]
+    
+    def update_all_and_get_data_array(self, bio_var: HR_BIO_VARIABLE):
+        self.check_buffer_sizes()
+        self.update_bpm()
         rr_intervals = self._get_rr_intervals()
         if rr_intervals != None:
-            poincare_feats = get_poincare_plot_features(nn_intervals=rr_intervals)
-            return poincare_feats['ratio_sd2_sd1']
-        return None
+            self.update_poincare(rr_intervals)
+            self.update_rmssd(rr_intervals)
+            self.update_sdnn(rr_intervals)
+
+        if bio_var == HR_BIO_VARIABLE.BPM:
+            return self.bpm_buffer
+        elif bio_var == HR_BIO_VARIABLE.POI_RAT:
+            return self.poincare_buffer
+        elif bio_var == HR_BIO_VARIABLE.RMSSD:
+            return self.rmssd_buffer
+        elif bio_var == HR_BIO_VARIABLE.SDNN:
+            return self.sdnn_buffer
+        else:
+            assert False
+
+
     
 
 
