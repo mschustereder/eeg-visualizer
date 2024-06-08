9e6b76941a302abfa29817c170ec49054171b9b7
Mathias Schustereder <schustereder@student.tugraz.at>
2024-06-08 11:54:30 +0200

    Add 3d visualizer and hr visualizer to main window (wip)

--- main.py
+++ main.py
@@ -1,24 +1,15 @@
+import sys
+from PySide6.QtWidgets import QApplication, QVBoxLayout, QWidget
+from visualizer.eeg_visualizer_main_window import EegVisualizerMainWindow
 import visualizer.globals as gl
 from signalProcessor.EEGProcessor import EEGProcessor
 from signalProcessor.HRProcessor import HRProcessor
-from PySide6.QtWidgets import QApplication, QMainWindow
 from visualizer.Visualizer3D import Visualizer3D
 from visualizer.VisualizerHR import VisualizerHR
-import sys
 from PySide6 import QtCore
-from visualizer.eeg_visualizer_main_window import EegVisualizerMainWindow
 
-def get_visualizer_widgets():
-    visualizer_3d = Visualizer3D()
-    visualizer_hr = VisualizerHR()
-    timer = QtCore.QTimer()
-    timer.timeout.connect(visualizer_3d.update_spectrum)
-    timer.timeout.connect(visualizer_hr.update_graph)
-    timer.setInterval(gl.EEG_GRAPH_INTERVAL_MS)
-    timer.start()
-    return visualizer_3d, visualizer_hr
 
-if __name__ == "__main__":
+def connect_to_streams():
     streams = gl.lsl_handler.get_all_lsl_streams()
     print([stream.name() for stream in streams])
     for stream in streams:
@@ -28,12 +19,31 @@ if __name__ == "__main__":
     gl.eeg_processor = EEGProcessor(gl.lsl_handler, gl.lsl_handler.get_stream_by_name("BrainVision RDA"))
     gl.hr_processor = HRProcessor(gl.lsl_handler, gl.lsl_handler.get_stream_by_name("HR_Polar H10 CA549123"), gl.lsl_handler.get_stream_by_name("RR_Polar H10 CA549123"))
 
+if __name__ == "__main__":
+    connect_to_streams()
+
     app = QApplication(sys.argv)
+    window = EegVisualizerMainWindow()
+    visualizer_3d = Visualizer3D()
+    visualizer_hr = VisualizerHR()
+    timer = QtCore.QTimer()
+    timer.timeout.connect(visualizer_3d.update_spectrum)
+    timer.timeout.connect(visualizer_hr.update_graph)
+    timer.setInterval(gl.EEG_GRAPH_INTERVAL_MS)
+    timer.start()
 
-    visualizer_3d, visualizer_hr = get_visualizer_widgets()
+    layout = QVBoxLayout()
 
-    window = EegVisualizerMainWindow(visualizer_3d, visualizer_hr)
+    visualizer_3d.setFixedSize(QtCore.QSize(400, 300))
+    visualizer_hr.setFixedSize(QtCore.QSize(400, 300))
 
+    layout.addWidget(visualizer_3d)
+    layout.addWidget(visualizer_hr)
+
+    container_widget = QWidget()
+    container_widget.setLayout(layout)
+
+    window.setCentralWidget(container_widget)
     window.show()
 
     sys.exit(app.exec())
--- visualizer/eeg_visualizer_main_window.py
+++ visualizer/eeg_visualizer_main_window.py
@@ -1,18 +1,7 @@
-from PySide6.QtCore import QSize, Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-
+from PySide6.QtWidgets import QMainWindow
 
 class EegVisualizerMainWindow(QMainWindow):
-    def __init__(self, visualizer_3d, visualizer_hr):
+    def __init__(self):
         super().__init__()
 
         self.setWindowTitle("EEG Visualizer Main Window")
-
-        layout = QVBoxLayout()
-
-        layout.addWidget(visualizer_3d)
-        layout.addWidget(visualizer_hr)
-
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
