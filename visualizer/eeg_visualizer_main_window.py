e019b0b399ab3c9822c29922786b77351f91515c
Mathias Schustereder <schustereder@student.tugraz.at>
2024-06-08 12:14:01 +0200

    Simplify and encapsulate UI bootstrap code (wip)

--- visualizer/eeg_visualizer_main_window.py
+++
@@ -1,7 +0,0 @@
-from PySide6.QtWidgets import QMainWindow
-
-class EegVisualizerMainWindow(QMainWindow):
-    def __init__(self):
-        super().__init__()
-
-        self.setWindowTitle("EEG Visualizer Main Window")
