dfa6fe34ec6521308062206bfca5d451808f5ff3
Felix Gaisberger <f.gaisberger@student.tugraz.at>
2024-06-11 18:53:57 +0200

    refactored lslhandler

--- lslHandler/lslHandler.py
+++ lslHandler/lslHandler.py
@@ -7,14 +7,15 @@ from dataclasses import dataclass, field
 from typing import Dict, Tuple, List
 from datetime import datetime
 import time
+import numpy as np
 
 SAMPLE_COUNT_MAX_QUEUE = 1000
 
-@dataclass
-class DataQueueStruct:
-    data_queue : queue.Queue = field(default_factory=queue.Queue)
-    data_queue_lock : threading.Lock = field(default_factory=threading.Lock)
-    queue_length : int = 0
+# @dataclass
+# class DataQueueStruct:
+#     data_queue : queue.Queue = field(default_factory=queue.Queue)
+#     data_queue_lock : threading.Lock = field(default_factory=threading.Lock)
+#     queue_length : int = 0
 
 class LslHandler:
     def __init__(self):
@@ -32,58 +33,44 @@ class LslHandler:
             info_string_list.append(string_of_list)
 
         return info_string_list
-    def data_recording_thread(self, stream : StreamInfo):
-        queue = self.active_streams[stream][1]
-        while not self._stop_event.is_set(): 
-                with queue.data_queue_lock:
-                    data, timestamps = self.active_streams[stream][0].pull_chunk(timeout=0.0, max_samples = 10000)
-                    data_with_timestamps = []
-                    data_with_timestamps.extend(zip(data, timestamps))
-
-                    if data:
-                        for element in data_with_timestamps:
-                            queue.data_queue.put(element)
-                        # print(f"added {data} to queue of stream {self.get_stream_name(stream)}")
-                        queue.queue_length += 1
-                    if queue.queue_length > SAMPLE_COUNT_MAX_QUEUE:
-                        print("Warning: Throwing away data samples")
-                        while not queue.data_queue.empty(): #this mechanism is probably not really efficient and should be overthought
-                            queue.data_queue.get(block= False)
-                            queue.data_queue.task_done()
-                        queue.queue_length = 0
-                time.sleep(0.0001)
-
+    
     def connect_to_specific_lsl_stream(self, stream : StreamInfo):
         new_inlet = pylsl.StreamInlet(stream)
-        self.active_streams[stream] = (new_inlet, DataQueueStruct())
+        self.active_streams[stream] = new_inlet
+        
+    def get_available_data(self, stream: StreamInfo, max_samples = SAMPLE_COUNT_MAX_QUEUE) -> List[Tuple[List[float], float]]:
+        inlet = self.active_streams[stream]
+        samples, timestamps = inlet.pull_chunk(timeout = 0.0, max_samples = max_samples)
+        if not samples: return None
+        data_with_timestamps = self._zip_samples_and_timestamps(samples, timestamps)
+        if inlet.samples_available() > SAMPLE_COUNT_MAX_QUEUE:
+            nr_of_flushed_samples = inlet.flush()
+            print(f"Warning: Throwing away {nr_of_flushed_samples} samples")
+        return data_with_timestamps
     
-    def start_data_recording_thread(self, stream: StreamInfo):
-        assert stream in self.active_streams
-        self.data_thread = threading.Thread(target=self.data_recording_thread, args=(stream,))
-        self.data_thread.start()
+    def get_available_data_without_timestamps(self, stream: StreamInfo, max_samples = SAMPLE_COUNT_MAX_QUEUE) -> List[List[float]]:
+        list = self.get_available_data(stream, max_samples)
+        if not list: return None
+        return [element[0] for element in list]
+    
+    def get_specific_amount_of_samples(self, stream: StreamInfo, required_sample_count) -> List[Tuple[List[float], float]]:
+        data = []
+        while (length := len(data)) < required_sample_count:
+            data_temp = self.get_available_data(stream, required_sample_count - length)
+            if data_temp:
+                data += data_temp
+        assert len(data) == required_sample_count
+        return data
 
-    def get_data_sample(self, stream: StreamInfo):
-        queue = self.active_streams[stream][1]
-        data_sample = None
-        with queue.data_queue_lock:
-            try:
-                data_sample = queue.data_queue.get(block = False) #.get() normally blocks which leads to a deadlock, therfore we set block to false and accept the exception if the queue is empty
-                queue.data_queue.task_done()
-                queue.queue_length -= 1
-            except: 
-                pass
-        return data_sample #if the queue was empty it returns None
-    
-    def get_available_data_as_chunk(self, stream : StreamInfo, max_samples = 1024):
-        chunk_to_return = []
-        queue_struct = self.active_streams[stream][1]
-        with queue_struct.data_queue_lock: 
-            while not queue_struct.data_queue.empty() and len(chunk_to_return) < max_samples: 
-                chunk_to_return.append(queue_struct.data_queue.get(block = False))
-                queue_struct.data_queue.task_done()
-            queue_struct.queue_length = 0
-        return chunk_to_return #returns empty list if queue is empty
-    
+    def get_specific_amount_of_samples_without_timestamps(self, stream: StreamInfo, required_sample_count) -> List[List[float]]:
+        list = self.get_specific_amount_of_samples(stream, required_sample_count)
+        return [element[0] for element in list]
+
+    def _zip_samples_and_timestamps(self, samples : List[List[float]], timestamps : List[float])  -> List[Tuple[List[float], float]]:
+        data_with_timestamps = []
+        data_with_timestamps.extend(zip(samples, timestamps))
+        return data_with_timestamps
+   
     def get_stream_by_name(self, name):
         for stream in self.active_streams:
             if stream.name() == name:
@@ -94,9 +81,7 @@ class LslHandler:
     def get_inlet(self,stream : StreamInfo) -> StreamInlet:
         return self.active_streams[stream][0]
     
-    def disconnect_and_stop_recording_thread(self, stream : StreamInfo):
-        self._stop_event.set()
-        self.data_thread.join()
+    def disconnect(self, stream : StreamInfo):
         self.get_inlet(stream).close_stream()
         del self.active_streams[stream]
     
